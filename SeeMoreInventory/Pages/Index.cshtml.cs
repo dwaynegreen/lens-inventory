5a5a9651651abb3aa243b275a0abbfb20993abdc
Dwayne Green <dgreen@1800contacts.com>
2017-09-20 17:10:16 -0600

    Updated Index page for Product Label validation and Quantity Validation

--- SeeMoreInventory/Pages/Index.cshtml.cs
+++ SeeMoreInventory/Pages/Index.cshtml.cs
@@ -41,5 +41,7 @@ namespace SeeMoreInventory.Pages
 
             await OnGetAsync();
         }
+
+        
     }
 }
---
+++ SeeMoreInventory/obj/Release/netcoreapp2.0/PubTmp/Out/wwwroot/js/addLabel.js
@@ -0,0 +1,26 @@
+﻿$('.AddLabel').on('click',
+    function () {
+        var LabelNumber = $(this).data('value');
+        $.each($('.labelTable tr td'),
+            function (index, value) {
+                if ($(this).find('input[type=checkbox]').is(':checked')) {
+                } else {
+                    if ($(this).find('.labelSticker').val() == '') {
+                        $(this).find('.labelSticker').val(LabelNumber);
+                        return false;
+                    }
+                }
+            });
+    });
+
+function clearSticker(id) {
+    var sheetValue = id.substring(3, 4);
+    if (id === 'cbx' + sheetValue) {
+        ($('#cbxSheet' + sheetValue).text(''));
+    }
+}
+
+function clearAll() {
+    $('input[type=checkbox]').prop('checked', false);
+    $('.labelSticker').val('');
+}
