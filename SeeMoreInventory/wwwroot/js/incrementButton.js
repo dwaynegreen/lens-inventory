3cdf2d4ee6c698cb4aba3e4b6bb409d324644d26
Dwayne Green <dgreen@1800contacts.com>
2017-10-15 20:07:18 -0600

    Fixed Increment and Decrement javascript and LabelSheet "No Sticker" removing value from label box

--- SeeMoreInventory/wwwroot/js/incrementButton.js
+++ SeeMoreInventory/wwwroot/js/incrementButton.js
@@ -1,13 +1,13 @@
 ﻿$(document).ready(function () {
-    var counter = $('#updateInventory_count').val();
+    var counter = $('#updateInventory_Count').val();
     $('#plus').click(function () {
-        var counter = $('#updateInventory_count').val();
+        var counter = $('#updateInventory_Count').val();
         counter++;
-        $('#updateInventory_count').val(counter);
+        $('#updateInventory_Count').val(counter);
     });
     $('#minus').click(function () {
-        var counter = $('#updateInventory_count').val();
+        var counter = $('#updateInventory_Count').val();
         counter--;
-        $('#updateInventory_count').val(counter);
+        $('#updateInventory_Count').val(counter);
     });
 });
